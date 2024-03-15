2481b510200c8fcb98fd9dc559de4a27081edc5a
Akshay Kumar <akshay.official.dev@gmail.com>
2024-03-16 00:04:44 +0530

    added design changes

--- src/screens/dashboards/kartik-parikrama/components/Search.tsx
+++ src/screens/dashboards/kartik-parikrama/components/Search.tsx
@@ -4,7 +4,7 @@ import { Input } from "@/components/ui/input";
 
 const Search = () => {
   return (
-    <div>
+    <div className="pt-20">
       <div className="flex flex-col justify-center items-center mb-8 space-y-4">
         <h2 className="text-2xl md:text-4xl font-bold text-center">
           Kartik Parikrama 2023
@@ -12,25 +12,17 @@ const Search = () => {
         <img src={DashboardImage} className="rounded-xl" alt="" />
       </div>
       <div>
-        <div className="px-4 flex justify-center items-center md:px-20 lg:px-36 space-y-4">
-          <SearchIcon className="h-5 w-5 mr-1 mt-2" />
+        <div className="px-4 flex justify-center items-center md:px-24 lg:px-48 space-y-4">
+          <SearchIcon className="h-5 w-5 mr-1 mt-4" />
           <Input placeholder="Search.." />
         </div>
-        <div className="px-12 flex justify-center items-center md:px-20 lg:px-36 space-y-4 mt-4">
-          <ul className="list-disc text-wrap">
-            <li>(default by name/email/city)</li>
-            <li>
-              for custom query: eg:name:jayendran + status:confirmed + city:
-              austin
-            </li>
-            <li>
-              name(either legal or
-              spiritual)/license/building/room/parking/gender/city/registration/email/
-              status(either regn or check-in
-              status)/needs/state/comments/spdisciple
-            </li>
-          </ul>
-        </div>
+        <p className="text-muted-foreground font-light text-sm md:text-lg text-center px-8 sm:px-16 md:px-24 lg:px-40 mt-2">
+          (default by name/email/city) <br /> For Custom Query:
+          eg:name:jayendran + status:confirmed + city: austin <br /> Available
+          Field Names: name(either legal or
+          spiritual)/license/building/room/parking/gender/city/registration/email/
+          status(either regn or check-in status)/needs/state/comments/spdisciple
+        </p>
       </div>
     </div>
   );
--- src/screens/dashboards/kartik-parikrama/page.tsx
+++ src/screens/dashboards/kartik-parikrama/page.tsx
@@ -8,7 +8,7 @@ const kartikParikrama = () => {
       <div className="hidden h-full md:flex md:w-80 md:flex-col md:fixed md:inset-y-0 z-[80] bg-gray-900">
         <Sidebar />
       </div>
-      <main className="md:pl-80 bg-slate-100 min-h-full">
+      <main className="md:pl-80 bg-slate-100 min-h-screen">
         <Navbar />
         <Search />
       </main>
