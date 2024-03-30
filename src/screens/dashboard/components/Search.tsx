eb190f0d097184ab3c601a9217f12c7fb17417c5
Akshay Kumar <akshay.official.dev@gmail.com>
2024-03-30 13:36:12 +0530

    Added small changes

--- src/screens/dashboard/components/Search.tsx
+++ src/screens/dashboard/components/Search.tsx
@@ -1,7 +1,23 @@
+import dashboardBanner from "../../../assets/dashboard.jpg";
+
 const Search = () => {
   return (
     <div>
-      <h1>Search</h1>
+      <div className="mb-8 space-y-4">
+        <h2 className="text-2xl md:text-4xl font-bold text-center">
+          Kartik Parikrama 2023
+        </h2>
+        <p className="text-muted-foreground font-light text-sm md:text-lg text-center">
+          Welcome to Sadhu Sanga Retreat Portal
+        </p>
+      </div>
+      <div className="px-4 md:px-20 lg:px-32 space-y-4 flex items-center justify-center">
+        <img
+          className="rounded-lg"
+          src={dashboardBanner}
+          alt="dashboard-banner"
+        />
+      </div>
     </div>
   );
 };
