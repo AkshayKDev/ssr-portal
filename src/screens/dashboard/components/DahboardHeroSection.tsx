062e5062b36768b373f23095d7eeb9a8180c2b52
Akshay Kumar <akshay.official.dev@gmail.com>
2024-07-11 00:07:11 +0530

    added routing custom content changes

--- src/screens/dashboard/components/DahboardHeroSection.tsx
+++ src/screens/dashboard/components/DahboardHeroSection.tsx
@@ -1,13 +1,52 @@
 import { Input } from "@/components/ui/input";
 import dashboardBanner from "../../../assets/dashboard.jpg";
 import { Search } from "lucide-react";
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+interface Event {
+  eventId: string;
+  eventName: string;
+}
 
 const DahboardHeroSection = () => {
+  const { eventId } = useParams<{ eventId: string }>();
+  const [events, setEvents] = useState<Event[]>([]);
+  const [selectedEvent, setSelectedEvent] = useState<Event | null>(null);
+  // const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    axios
+      .get(
+        "http://p401.network.sadhu-sanga.appspot.com/getEventsList?apiVersion=2.9"
+      )
+      .then((response) => {
+        setEvents(response.data.eventsList);
+        // setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching event details:", error);
+        // setLoading(false);
+      });
+  }, []);
+
+  useEffect(() => {
+    if (events?.length > 0) {
+      const event = events.find((event) => event.eventId === eventId);
+      setSelectedEvent(event || null);
+    }
+    console.log(eventId);
+  }, [eventId, events]);
+
+  console.log(events);
+  console.log(eventId);
+
   return (
     <div>
       <div className="mb-4 space-y-4">
         <h2 className="text-2xl md:text-4xl font-bold text-center">
-          Kartik Parikrama 2023
+          {selectedEvent?.eventName}
         </h2>
         <p className="text-muted-foreground font-light text-sm md:text-lg text-center">
           Welcome to Sadhu Sanga Retreat Portal
