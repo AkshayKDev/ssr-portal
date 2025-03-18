bf1b2f57ebc7f73fec51ffa98ee670396c9106c1
Akshay Kumar <akshay.official.dev@gmail.com>
2025-03-18 23:35:37 +0530

    added tailiwnd v4

--- src/screens/Home/components/Hero.tsx
+++ src/screens/Home/components/Hero.tsx
@@ -24,14 +24,14 @@ const Hero = () => {
   return (
     <div className="flex items-center justify-center flex-col">
       <div className="flex items-center justify-center flex-col">
-        <div className="mb-4 flex font-bold items-center border shadow-sm p-4 bg-amber-100 text-amber-700 rounded-full uppercase">
+        <div className="mb-4 flex font-bold items-center border shadow-xs p-4 bg-amber-100 text-amber-700 rounded-full uppercase">
           <UserRound className="h-6 w-6 mr-2" />
           Admin Dashboard
         </div>
         <h1 className="text-3xl md:text-6xl text-center text-neutral-800 mb-6 font-bold">
           Sadhu Sanga Retreat Portal
         </h1>
-        <div className="text-3xl md:text-6xl font-bold bg-gradient-to-r from-fuchsia-600 to-pink-600 text-white px-4 p-2 rounded-md pb-4 w-fit mb-4">
+        <div className="text-3xl md:text-6xl font-bold bg-linear-to-r from-fuchsia-600 to-pink-600 text-white px-4 p-2 rounded-md pb-4 w-fit mb-4">
           Hare Krsna
         </div>
       </div>
--- src/screens/Home/components/Navbar.tsx
+++ src/screens/Home/components/Navbar.tsx
@@ -3,8 +3,8 @@ import Prabhupad from "../../../assets/prabhupad.png";
 
 const Navbar = () => {
   return (
-    <div className="fixed top-0 w-full h-20 px-4 border-b shadow-sm bg-white flex items-center">
-      <div className="md:max-w-screen-2xl mx-auto flex items-center w-full justify-between">
+    <div className="fixed top-0 w-full h-20 px-4 border-b shadow-xs bg-white flex items-center">
+      <div className="md:max-w-(--breakpoint-2xl) mx-auto flex items-center w-full justify-between">
         <Logo />
         <img src={Prabhupad} className="h-20 w-20 object-contain" alt="" />
       </div>
